38531bbec07eba7cc3e2472b5a6750e2137757a3
froggagul <froggagul@users.noreply.github.com>
2021-07-03 16:57:26 +0000

    deploy: 777374795bb78f2b2560be4ec2bb1b803275f621

---
+++ component---src-pages-404-tsx-3ae7a50a4765308a80ca.js
@@ -0,0 +1,2 @@
+(self.webpackChunkhojin_s_note=self.webpackChunkhojin_s_note||[]).push([[218],{9786:function(e,n,t){"use strict";t.r(n);var l=t(7294);n.default=function(){return console.log("404"),l.createElement(l.Fragment,null,l.createElement("h1",null,"404"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-3ae7a50a4765308a80ca.js.map
